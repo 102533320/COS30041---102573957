d8e4e965fd46093540db5fb3523071edbad4e135
Matthew Coulter <mattcoul7@gmail.com>
2022-03-02 15:26:41 +1100

    Updates

--- Lab 1/VehicleHireApp/src/vehiclehireapp/VehicleHireApp.java
+++ Lab 1/VehicleHireApp/src/vehiclehireapp/VehicleHireApp.java
@@ -16,26 +16,5 @@ public class VehicleHireApp {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
-    private String code;
-    private String description;
-    private Integer seats;
-    public VehicleType(String code, String description, Integer seats) {
-        this.code = code;
-        this.description = description;
-        this.seats = seats;
-    }
-    public String getCode() {
-        return code.toUpperCase();
-    }
-    public void setCode(String code) {
-        this.code = code;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    
+
 }
