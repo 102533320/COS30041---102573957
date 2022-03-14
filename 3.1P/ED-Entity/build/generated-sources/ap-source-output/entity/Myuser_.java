3aca55650c29276842145d5180358b2e28fcc00f
Matthew Coulter <mattcoul7@gmail.com>
2022-03-14 12:05:19 +1100

    Fixed project dependencies to c drive

---
+++ 3.1P/ED-Entity/build/generated-sources/ap-source-output/entity/Myuser_.java
@@ -0,0 +1,20 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-03-14T11:55:45")
+@StaticMetamodel(Myuser.class)
+public class Myuser_ { 
+
+    public static volatile SingularAttribute<Myuser, String> password;
+    public static volatile SingularAttribute<Myuser, String> address;
+    public static volatile SingularAttribute<Myuser, String> phone;
+    public static volatile SingularAttribute<Myuser, String> name;
+    public static volatile SingularAttribute<Myuser, String> userid;
+    public static volatile SingularAttribute<Myuser, String> secqn;
+    public static volatile SingularAttribute<Myuser, String> email;
+    public static volatile SingularAttribute<Myuser, String> secans;
+
+}
